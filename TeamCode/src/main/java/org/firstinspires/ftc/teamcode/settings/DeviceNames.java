0ebbea78a91de2345f06f8273c107e3abea0c1a7
Joshua Miller <82299111+22jmiller@users.noreply.github.com>
2021-05-07 15:08:37 -0400

    Revert "Update branch"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/settings/DeviceNames.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.settings;
-
-public class DeviceNames {
-    public static final String[] LEFT_DRIVE = {"ld1","ld2"};
-    public static final String[] RIGHT_DRIVE = {"rd1","rd2"};
-
-}
