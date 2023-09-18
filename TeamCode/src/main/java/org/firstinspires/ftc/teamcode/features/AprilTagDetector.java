63e72f69ed2bd47824da9f551bd5d347624256b9
Michael Lachut <michael@lachut.com>
2023-09-18 16:12:13 -0400

    Drive Towards April Tag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/features/AprilTagDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/features/AprilTagDetector.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.features;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.internals.features.Buildable;
 import org.firstinspires.ftc.teamcode.internals.features.Feature;
 import org.firstinspires.ftc.teamcode.internals.hardware.Devices;
@@ -24,6 +26,7 @@ public class AprilTagDetector extends Feature implements Buildable {
     public void build() {
         // Build AprilTag Detector
         aprilTag = new AprilTagProcessor.Builder()
+                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
                 .build();
 
         vision = new VisionPortal.Builder()
@@ -39,4 +42,20 @@ public class AprilTagDetector extends Feature implements Buildable {
     }
 
     public List<AprilTagDetection> getCurrentDetections() { return currentDetections; }
+
+    public void stop() {
+        vision.setProcessorEnabled(aprilTag, false);
+    }
+
+    public void start() {
+        vision.setProcessorEnabled(aprilTag, true);
+    }
+
+    public void resumeStreaming() {
+        vision.resumeLiveView();
+    }
+
+    public void stopStreaming() {
+        vision.stopLiveView();
+    }
 }
