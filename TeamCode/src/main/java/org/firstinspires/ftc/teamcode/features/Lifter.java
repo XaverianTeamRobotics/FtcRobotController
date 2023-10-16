818b1ae2a4970cf4fc191866d9a7fe0ddf72b1d9
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2023-10-16 15:26:14 -0400

    Reverse the direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/features/Lifter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/features/Lifter.java
@@ -6,7 +6,7 @@ import org.firstinspires.ftc.teamcode.internals.hardware.Devices;
 public class Lifter extends Feature {
     @Override
     public void loop() {
-        double motorSpeed = Devices.controller1.getLeftStickY();
+        double motorSpeed = -Devices.controller1.getLeftStickY();
         Devices.motor0.setSpeed(motorSpeed);
     }
 }
