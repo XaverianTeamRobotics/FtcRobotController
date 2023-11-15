25c09d95fb39e43ca397029f2f03ea98567755f7
Marco Petrino <marco3.14p@gmail.com>
2023-11-15 15:38:39 -0500

    Did Someone Say Airplanes?  I'm Michael Lachut and I fly airplanes.  Vrrrrreeewwwwwww  **Explosion Sound**
    By the way, this runs the airplane lancher.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DidSomeoneSayAirplanes.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import org.firstinspires.ftc.teamcode.internals.hardware.Devices;
+import org.firstinspires.ftc.teamcode.internals.registration.OperationMode;
+import org.firstinspires.ftc.teamcode.internals.registration.TeleOperation;
+
+public class DidSomeoneSayAirplanes extends OperationMode implements TeleOperation {
+    @Override
+    public void construct() {
+
+    }
+
+    @Override
+    public void run() {
+        double hold;
+        hold = 0;
+        double release;
+        release = 50;
+        Devices.servo0.setPosition(hold);
+        if (Devices.controller1.getCircle()) {
+            Devices.servo0.setPosition(release);
+            //circle button will release the airplane
+        }
+        if (Devices.controller1.getCross()) {
+            Devices.servo0.setPosition(hold);
+            //cross button will hold the elastic in place
+        }
+    }
+}
