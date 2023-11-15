f06b3fa23d6223f4eda2263bfbeddd711f4cded9
Marco Petrino <marco3.14p@gmail.com>
2023-11-15 16:48:49 -0500

    This runs the arm, claw, claw pivot, and intake.  Ignore the fact that I had to use an entire method for the intake - it was initially supposed to do more than that.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/features/ArmClawIntake.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.features;
+
+import static org.firstinspires.ftc.teamcode.internals.hardware.Devices.motor2;
+import static org.firstinspires.ftc.teamcode.internals.hardware.Devices.servo0;//this is the left grabber
+import static org.firstinspires.ftc.teamcode.internals.hardware.Devices.servo1;//this is the right grabber
+import static org.firstinspires.ftc.teamcode.internals.hardware.Devices.servo2;//this is the servo for the claw mechanism
+
+import org.firstinspires.ftc.teamcode.internals.hardware.Devices;//our two devices our the motors for the arm
+import org.firstinspires.ftc.teamcode.internals.registration.OperationMode;
+import org.firstinspires.ftc.teamcode.internals.registration.TeleOperation;
+
+public class ArmClawIntake extends OperationMode implements TeleOperation {
+    public int intakeCount() {
+        motor2.setPower(0);
+        double counter = 0;
+        if (Devices.controller1.getDpadDown()) {
+            counter += 1;
+        }
+        if (counter % 2 != 0) {
+            return 100;
+        }
+        if (counter % 2 == 0) {
+            return 0;
+        } else {
+            return 0;
+        }
+    }
+    @Override
+    public void construct () {
+        servo0.setPosition(20);
+        servo1.setPosition(80);
+        servo2.setPosition(0);
+    }
+
+    @Override
+    public void run () {
+        double motorPower = (Devices.controller1.getRightTrigger() - Devices.controller1.getLeftTrigger());
+        Devices.motor0.setPower(motorPower);
+        Devices.motor1.setPower(-motorPower);
+
+        //the motors must move in opposite directions
+        if (Devices.controller1.getDpadLeft()) {
+            servo0.setPosition(5);
+            //closes the left grabber
+        }
+        if (Devices.controller1.getDpadRight()) {
+            servo1.setPosition(100);
+            //closes the right grabber
+        }
+        if (Devices.controller1.getDpadUp()) {
+            servo0.setPosition(20);
+            servo1.setPosition(80);
+            //opens both grabbers
+        }
+        if (Devices.controller1.getLeftBumper()) {
+            servo2.setPosition(30);
+            //this will rotate the entire claw mechanism so that it is in line with the backboard
+        }
+        if (Devices.controller1.getRightBumper()) {
+            servo2.setPosition(0);
+            //this will return the claw mechanism back to the initial position
+        }
+        if (Devices.controller1.getRightBumper()) {
+            Devices.motor2.setPower(50);
+        }
+        Devices.motor2.setPower(intakeCount());
+    }
+}
