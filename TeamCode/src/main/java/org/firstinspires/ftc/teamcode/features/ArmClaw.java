91e0d7268d33805cad6ccbcde01877a54181f366
Michael Lachut <michael@lachut.com>
2023-11-18 08:30:06 -0500

    emergency fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/features/ArmClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/features/ArmClaw.java
@@ -18,11 +18,13 @@ import org.firstinspires.ftc.teamcode.internals.hardware.Devices;//our two devic
  * All controls are in controller 2
  */
 public class ArmClaw extends Feature implements Buildable {
+    public static final int R_OPEN = 83, L_OPEN = 20;
+    public static final int R_CLOSED = 100, L_CLOSED = 5;
     private double counter = 0;
     private boolean dpadPressed = false;
     public void build(){
-        servo0.setPosition(20);
-        servo1.setPosition(80);
+        servo0.setPosition(L_OPEN);
+        servo1.setPosition(R_OPEN);
         servo2.setPosition(0);
     }
     public int intakeCount() {
@@ -31,36 +33,42 @@ public class ArmClaw extends Feature implements Buildable {
             dpadPressed = true;
         } else if (!Devices.controller2.getDpadDown()) dpadPressed = false;
         if (counter % 2 != 0) {
-            return 100;
+            return R_CLOSED;
         } else {
             return 0;
         }
     }
     public void loop() {
-        double motorPower = (Devices.controller2.getRightTrigger() - Devices.controller2.getLeftTrigger());
+        double motorPower = -(Devices.controller2.getRightTrigger() - Devices.controller2.getLeftTrigger()) * 0.75;
         Devices.motor0.setPower(motorPower);
-        Devices.motor1.setPower(-motorPower);
+        Devices.motor1.setPower(motorPower);
 
         //the motors must move in opposite directions
         if (Devices.controller2.getDpadLeft()) {
-            servo0.setPosition(5);
+            servo0.setPosition(L_CLOSED);
             //closes the left grabber
         }
         if (Devices.controller2.getDpadRight()) {
-            servo1.setPosition(100);
+            servo1.setPosition(R_CLOSED);
             //closes the right grabber
         }
         if (Devices.controller2.getDpadUp()) {
-            servo0.setPosition(20);
-            servo1.setPosition(80);
+            servo0.setPosition(L_OPEN);
+            servo1.setPosition(R_OPEN);
             //opens both grabbers
         }
+        if (Devices.controller2.getSquare()) {
+            servo0.setPosition(L_OPEN);
+        }
+        if (Devices.controller2.getCircle()) {
+            servo1.setPosition(R_OPEN);
+        }
         if (Devices.controller2.getLeftBumper()) {
-            servo2.setPosition(33);
+            servo2.setPosition(servo2.getPosition() + 0.5);
             //this will rotate the entire claw mechanism so that it is in line with the backboard
         }
         if (Devices.controller2.getRightBumper()) {
-            servo2.setPosition(0);
+            servo2.setPosition(servo2.getPosition() - 0.5);
             //this will return the claw mechanism back to the initial position
         }
         Devices.motor2.setPower(-intakeCount());
