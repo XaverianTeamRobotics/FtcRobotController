831b5303e7112085cea3b0720af0121a2bf55de3
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2023-11-28 15:14:25 -0500

    Add apk webinstaller build to the docs build script

--- HelpPage/scripts/build.sh
+++ HelpPage/scripts/build.sh
@@ -11,6 +11,14 @@ npm install
 npm run build
 echo Docusaurus build complete.
 
+echo Building apk webinstaller...
+mkdir ../apk
+cd ../apk
+git clone https://github.com/XaverianTeamRobotics/webinstaller .
+npm install
+npm run build
+echo Webinstaller build complete.
+
 echo Building imgutil...
 cd ../imgutil
 npm install
@@ -22,6 +30,8 @@ cd ..
 mkdir output
 # Move docusaurus build to final output
 mv ./doc/build ./build
+# Move webinstaller build to final output
+mv ./apk/build ./build/apk
 # Move imgutil build to final output
 mv ./imgutil/dist ./build/imgutil
 # Move static files to final output
