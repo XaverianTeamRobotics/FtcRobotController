4f0e58dae74fc1a5d462d0ac816eb0e43ea0a62a
liamfeeney <25lfeeney@xbhs.net>
2023-12-05 10:25:03 -0500

    added BestPathFinder.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/internals/motion/auto_auto/BestPathFinder.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.internals.motion.auto_auto;
+
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class BestPathFinder {
+    private ArrayList<ArrayList<AutoAutoPathSegment>> allPaths;
+    private ArrayList<AutoAutoPathSegment> pathSegments;
+
+    public ArrayList<ArrayList<AutoAutoPathSegment>> getAllPaths() {
+        ArrayList<ArrayList<AutoAutoPathSegment>> allPaths = new ArrayList<>();
+
+        int duplicates = 0;
+        while (duplicates < 100) {
+            // Instance of random class
+            Random rand = new Random();
+
+            int length = rand.nextInt(pathSegments.size()) + 1;
+            ArrayList<AutoAutoPathSegment> path = new ArrayList<>();
+            for (int i = 0; i < length; i++) {
+                int index = rand.nextInt(pathSegments.size());
+                AutoAutoPathSegment segment = pathSegments.get(index);
+                path.add(segment);
+            }
+
+            if (allPaths.contains(path)) {
+                duplicates++;
+            } else {
+                allPaths.add(path);
+            }
+        }
+        this.allPaths = allPaths;
+        return allPaths;
+    }
+}
