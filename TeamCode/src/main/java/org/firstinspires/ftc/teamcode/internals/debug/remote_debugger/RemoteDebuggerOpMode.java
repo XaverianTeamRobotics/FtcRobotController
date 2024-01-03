aeda10563aaa600819a5208d1334ff0ebaddef54
Michael Lachut <michael@lachut.com>
2024-01-03 15:19:44 -0500

    changed servos on rd (COLIN!!!!)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/internals/debug/remote_debugger/RemoteDebuggerOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/internals/debug/remote_debugger/RemoteDebuggerOpMode.java
@@ -8,7 +8,7 @@ public class RemoteDebuggerOpMode extends OperationMode implements TeleOperation
     @Override
     public void construct() {
         RDWebSocketServer.enableMotorStatic(0, 3);
-        RDWebSocketServer.enableServoStatic(0, 6);
+        RDWebSocketServer.enableServoStatic(0, 5);
 
         registerFeature(new Debugger());
     }
