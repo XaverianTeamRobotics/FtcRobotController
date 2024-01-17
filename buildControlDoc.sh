4c9eb31bce004e4b7375f97c2443b636aa7077d0
Michael Lachut <michael@lachut.com>
2024-01-17 10:42:48 -0500

    Action for controls build

---
+++ buildControlDoc.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+APK_PATH=$1
+git clone https://github.com/XaverianTeamRobotics/FTC-DocParser
+cd FTC-DocParser || exit 1
+cp ../"$APK_PATH" ./APK.apk
+./unzipAPK.sh APK.apk
+./gradlew run
+cp ./doc-out/ ../doc-out/
+cd ..
+rm -rf FTC-DocParser
