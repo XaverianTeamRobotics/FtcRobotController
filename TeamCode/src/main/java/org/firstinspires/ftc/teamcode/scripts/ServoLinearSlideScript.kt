6d7a9385630de7c9033339cc9117caec078b15d4
michaell4438 <michael@lachut.com>
2024-09-24 10:08:38 -0400

    Added the ability to remap inputs for some scripts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scripts/ServoLinearSlideScript.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scripts/ServoLinearSlideScript.kt
@@ -4,7 +4,8 @@ import org.firstinspires.ftc.teamcode.internals.base.HardwareManager
 import org.firstinspires.ftc.teamcode.internals.base.HardwareManager.gamepad1
 import org.firstinspires.ftc.teamcode.internals.base.Script
 
-class ServoLinearSlideScript(id: Int = 0, private val inverted: Boolean = false) : Script() {
+class ServoLinearSlideScript(id: Int = 0, private val inverted: Boolean = false,
+                             private val input: () -> Double = { (gamepad1.right_trigger - gamepad1.left_trigger).toDouble() }) : Script() {
     private val servo = HardwareManager.servos.get("ls$id", 0)
 
     override fun init() {
@@ -13,8 +14,7 @@ class ServoLinearSlideScript(id: Int = 0, private val inverted: Boolean = false)
 
     override fun run() {
         while (true) {
-            var input = ((if (inverted) -1.0 else 1.0) * (gamepad1.right_trigger - gamepad1.left_trigger))
-            servo.position = (input/2) + 0.5
+            servo.position = (if (inverted) -1 else 1) * ((input()/2) + 0.5)
         }
     }
 
