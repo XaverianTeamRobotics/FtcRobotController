a9ab4c5701f3ff0dccfdca864b0b1f66bfb2764f
michaell4438 <michael@lachut.com>
2024-10-09 17:02:13 -0400

    camera code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scripts/MecanumDriveScript.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scripts/MecanumDriveScript.kt
@@ -7,33 +7,19 @@ import org.firstinspires.ftc.teamcode.internals.base.Script
 import kotlin.math.abs
 import kotlin.math.max
 
-class MecanumDriveScript : Script {
-    private val frontLeftMotor: DcMotor
-    private val backLeftMotor: DcMotor
-    private val frontRightMotor: DcMotor
-    private val backRightMotor: DcMotor
-
-    constructor() {
-        frontLeftMotor = motors.get("fl", 0)
-        backLeftMotor = motors.get("bl", 1)
-        frontRightMotor = motors.get("fr", 2)
-        backRightMotor = motors.get("br", 3)
-    }
-
-    constructor(frontLeftMotor: DcMotor, backLeftMotor: DcMotor, frontRightMotor: DcMotor, backRightMotor: DcMotor) {
-        this.frontLeftMotor = frontLeftMotor
-        this.backLeftMotor = backLeftMotor
-        this.frontRightMotor = frontRightMotor
-        this.backRightMotor = backRightMotor
-    }
+class MecanumDriveScript(private val powerScale: Double = 1.0, private val rotScale: Double = 1.0) : Script() {
+    private val frontLeftMotor: DcMotor = motors.get("fl", 0)
+    private val backLeftMotor: DcMotor = motors.get("bl", 1)
+    private val frontRightMotor: DcMotor = motors.get("fr", 2)
+    private val backRightMotor: DcMotor = motors.get("br", 3)
 
     override fun init() {}
 
     override fun run() {
         while (true) {
-            val y = gamepad1.left_stick_y.toDouble()
-            val x = gamepad1.left_stick_x.toDouble() * 1.1
-            val rx = gamepad1.right_stick_x.toDouble()
+            val y = -gamepad1.left_stick_y.toDouble() * powerScale
+            val x = gamepad1.left_stick_x.toDouble() * 1.1 * powerScale
+            val rx = gamepad1.right_stick_x.toDouble() * powerScale * rotScale
 
             val denominator = max(abs(y) + abs(x) + abs(rx), 1.0)
             val frontLeftPower = (y + x + rx) / denominator
