917ff4a484bbdab53acd25f43e6ade34112dff16
Marco Petrino <marco3.14p@gmail.com>
2024-10-23 07:41:42 -0400

    Hanging Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Hanging.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Hanging.kt
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode.opmodes
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.internals.base.BaseOpMode
-import org.firstinspires.ftc.teamcode.scripts.HangingScript
+import org.firstinspires.ftc.teamcode.internals.base.HardwareManager
+import org.firstinspires.ftc.teamcode.scripts.ContinuousMotorScript
 
 @TeleOp(name = "Hanging", group = BaseOpMode.DEBUG_GROUP_NAME)
 class Hanging: BaseOpMode() {
     override fun construct() {
-        addScript(HangingScript())
+        addScript(ContinuousMotorScript(id = 4, input = { HardwareManager.gamepad1.right_stick_y.toDouble() })) // Lifter
+        addScript(ContinuousMotorScript(id = 5, input = { HardwareManager.gamepad1.left_stick_y.toDouble() })) // Adjuster
     }
 
     override fun run() {
