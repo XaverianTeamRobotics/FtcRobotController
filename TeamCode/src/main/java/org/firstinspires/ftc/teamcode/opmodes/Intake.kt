79480b71e93ca63093fe8f25c11dc45f83e7fb26
Marco Petrino <marco3.14p@gmail.com>
2024-10-30 15:09:57 -0400

    Intake code for future intake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Intake.kt
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.opmodes
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.internals.hardware.HardwareManager
+import org.firstinspires.ftc.teamcode.internals.templates.BaseOpMode
+import org.firstinspires.ftc.teamcode.scripts.ContinuousServoScript
+
+
+@TeleOp(name = "Intake", group = BaseOpMode.DEBUG_GROUP_NAME)
+class Intake: BaseOpMode() {
+    override fun construct() {
+        addScript(ContinuousServoScript(id = 0, inverted = false, input = {
+            HardwareManager.gamepad1.left_trigger.toDouble() - HardwareManager.gamepad1.right_trigger.toDouble()
+        }))
+        addScript(ContinuousServoScript(id = 1, inverted = true, input = {
+            HardwareManager.gamepad1.left_trigger.toDouble() - HardwareManager.gamepad1.right_trigger.toDouble()
+        }))
+    }
+
+    override fun run() {
+    }
+
+    override fun onStop() {
+    }
+}
