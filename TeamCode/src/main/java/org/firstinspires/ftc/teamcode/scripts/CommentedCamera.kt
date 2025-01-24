e723b088a4c5543d2d99f3d11f1b009c6c9f69d6
Marco Petrino <marco3.14p@gmail.com>
2025-01-23 21:30:03 -0500

    Forgot to push this stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scripts/CommentedCamera.kt
@@ -0,0 +1,21 @@
+/**
+
+
+class HuskylensRotateScript {
+    /*
+     * private var servoInput
+     *
+     * val inputHook: () -> Double = { servoInput }
+     *
+     * update {
+     *      do logic
+     *      servoInput = something
+     * }
+     *
+     * -------------
+     *
+     * script = HuskylensRotateScript()
+     * ContinuousServoScript(id, input = script.inputHook)
+     */
+}
+ */
